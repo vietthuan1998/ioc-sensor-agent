a8745d7b69045006ff6e92bb89272475789d05d8
dvthuan1998 <vietthuan1998@gmail.com>
2026-05-26 09:14:12 +0700

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,32 +3,16 @@ module.exports = {
     {
       name: 'ioc-sensor-agent',
       script: 'src/main.js',
-      cwd: './current',
+      cwd: '/home/raspi/Project/ioc-sensor-agent',
       env_file: '.env',
       watch: false,
       autorestart: true,
       max_restarts: 10,
       min_uptime: '5s',
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
-      out_file: '../logs/sensor-agent-out.log',
-      error_file: '../logs/sensor-agent-err.log',
+      out_file: '/home/raspi/Project/ioc-sensor-agent/logs/sensor-agent-out.log',
+      error_file: '/home/raspi/Project/ioc-sensor-agent/logs/sensor-agent-err.log',
       merge_logs: true,
     },
   ],
-deploy: {
-    production: {
-      user: 'raspi',
-
-      host: 'raspi.local',
-
-      ref: 'origin/main',
-
-      repo: 'https://github.com/vietthuan1998/ioc-sensor-agent.git',
-
-      path: '/home/raspi/Project/ioc-sensor-agent',
-
-      'post-deploy':
-        'cd current && npm install && pm2 reload ecosystem.config.js --env production',
-    },
-  },
 };
